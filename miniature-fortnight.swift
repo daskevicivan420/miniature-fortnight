1795ce0a49372a27e78a0e3d118cedb5f6fffce5
Karen534 <151012545+daskevicivan420@users.noreply.github.com>
2023-12-15 02:20:06 +0700

    Create miniature-fortnight.swift

---
+++ miniature-fortnight.swift
@@ -0,0 +1,7 @@
+    case toggleGrid
+    case togglePlayCollage
+case removeShape(String)
+    case addText(TextSettings)
+    case changeText(TextModification, id: String)
+    case removeText(String)
+    case addSticker(Sticker)
